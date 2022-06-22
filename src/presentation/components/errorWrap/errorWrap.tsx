f83b59ea18d5d7c0f44147a20a7bdfaf93fc0354
DESKTOP-5GDNSP4\Joao <joao.silva@simberg.com.br>
2022-06-22 14:07:58 -0300

    feat: ensure login will not render login loader and error on first load

--- src/presentation/components/errorWrap/errorWrap.tsx
+++ src/presentation/components/errorWrap/errorWrap.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { faEllipsis } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from './errorWrap-styles.scss'
+import Context from '@/presentation/contexts/login-form/login-form-context'
 
 const ErrorWrap: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(Context)
   return (
-    <div className={styles.errorWrap}>
-      <FontAwesomeIcon beatFade className={styles.ellipsis} icon={faEllipsis} />
-      <span className={styles.error}>Erro</span>
+    <div data-testid='errorWrap' className={styles.errorWrap}>
+      {isLoading && <FontAwesomeIcon beatFade className={styles.ellipsis} icon={faEllipsis} />}
+      { errorMessage && <span className={styles.error}>{errorMessage}</span>}
     </div>
   )
 }
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './login-styles.scss'
 import { Header, ErrorWrap, Footer } from '@/presentation/components'
+import Context from '@/presentation/contexts/login-form/login-form-context'
+
+type FormStatusType = {
+  isLoading: boolean
+  errorMessage: string
+}
 
 const Login: React.FC = () => {
+  const [formStatus] = useState<FormStatusType>({
+    isLoading: false,
+    errorMessage: ''
+  })
+
   return (
     <div className={styles.login}>
       <Header />
-      <form className={styles.form}>
-        <h2>Login</h2>
-        <input autoComplete='off' type='email' name='email' placeholder='Digite seu email' />
-        <input autoComplete='off' type='password' name='password' placeholder='Digite sua senha' />
-        <button type="submit">Entrar</button>
-        <span className={styles.link}>Criar conta</span>
-        <ErrorWrap />
-      </form>
+      <Context.Provider value={formStatus}>
+        <form className={styles.form}>
+          <h2>Login</h2>
+          <input autoComplete='off' type='email' name='email' placeholder='Digite seu email' />
+          <input autoComplete='off' type='password' name='password' placeholder='Digite sua senha' />
+          <button type="submit">Entrar</button>
+          <span className={styles.link}>Criar conta</span>
+          <ErrorWrap />
+        </form>
+      </Context.Provider>
 
       <Footer />
     </div>
