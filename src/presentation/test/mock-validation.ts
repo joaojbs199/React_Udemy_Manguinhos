7ecd1e8d2e76f2f1fff129ecad3dc033679da337
DESKTOP-5GDNSP4\Joao <joao.silva@simberg.com.br>
2022-06-22 17:28:08 -0300

    test: ensure login present error if password invalid

--- src/presentation/test/mock-validation.ts
+++ src/presentation/test/mock-validation.ts
@@ -1,13 +1,9 @@
 import { Validation } from '@/presentation/protocols/validation'
 
-export class ValidationSpy implements Validation {
+export class ValidationStub implements Validation {
   errorMessage: string
-  field: string
-  value: string
 
   validate (field: string, value: string): string {
-    this.field = field
-    this.value = value
     return this.errorMessage
   }
 }
