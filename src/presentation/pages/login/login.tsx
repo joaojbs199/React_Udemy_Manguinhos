763e6971a77b8b459ee1d79edc56c960d8e2b1c2
DESKTOP-5GDNSP4\Joao <joao.silva@simberg.com.br>
2022-06-22 16:20:20 -0300

    feat: ensure login calls validation with correct email

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,26 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './login-styles.scss'
 import { Header, ErrorWrap, Footer } from '@/presentation/components'
 import Context from '@/presentation/contexts/login-form/login-form-context'
+import { Validation } from '@/presentation/protocols/validation'
 
-const Login: React.FC = () => {
-  const [formStatus] = useState({
+type Props = {
+  validation?: Validation
+}
+
+const Login: React.FC<Props> = ({ validation }: Props) => {
+  const [state, setState] = useState({
     isLoading: false,
+    email: '',
     mainError: '',
     emailError: 'Campo obrigatório',
     passwordError: 'Campo obrigatório'
   })
 
+  useEffect(() => {
+    validation.validate({ email: state.email })
+  }, [state.email])
+
+  const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value
+    })
+  }
+
   return (
     <div className={styles.login}>
       <Header />
-      <Context.Provider value={formStatus}>
+      <Context.Provider value={{ state, setState }}>
         <form className={styles.form}>
           <h2>Login</h2>
-          <input autoComplete='off' type='email' name='email' placeholder='Digite seu email' />
-          {formStatus.emailError && <span data-testid='emailError' className={styles.inputError}>{formStatus.emailError}</span>}
-          <input autoComplete='off' type='password' name='password' placeholder='Digite sua senha' />
-          {formStatus.passwordError && <span data-testid='passwordError' className={styles.inputError}>{formStatus.passwordError}</span>}
+          <input
+            data-testid='email'
+            autoComplete='off'
+            type='email'
+            name='email'
+            placeholder='Digite seu email'
+            onChange={handleChange}
+          />
+          {state.emailError && <span data-testid='emailError' className={styles.inputError}>{state.emailError}</span>}
+          <input
+            data-testid='password'
+            autoComplete='off'
+            type='password'
+            name='password'
+            placeholder='Digite sua senha'
+          />
+          {state.passwordError && <span data-testid='passwordError' className={styles.inputError}>{state.passwordError}</span>}
           <button className={styles.submit} data-testid='submit' disabled type="submit">Entrar</button>
           <span className={styles.link}>Criar conta</span>
           <ErrorWrap />
