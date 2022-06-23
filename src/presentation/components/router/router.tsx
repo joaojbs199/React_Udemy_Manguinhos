27954ecad1c50b9b3143805c52135c726d7926b5
DESKTOP-5GDNSP4\Joao <joao.silva@simberg.com.br>
2022-06-23 18:57:35 -0300

    refactor: move AuthenticationSpy to its own file

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from '@/presentation/pages'
-import { ValidationStub } from '@/presentation/test'
+import { AuthenticationSpy, ValidationStub } from '@/presentation/test'
 
 const Router: React.FC = () => {
   const validationStub = new ValidationStub()
+  const authenticationSpy = new AuthenticationSpy()
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<Login validation={validationStub} />} />
+        <Route path='/login' element={<Login validation={validationStub} authentication={authenticationSpy} />} />
       </Routes>
     </BrowserRouter>
   )
