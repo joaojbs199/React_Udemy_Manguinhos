b182c688fb6a6eb17edb5a25abd6da85171ac6d4
DESKTOP-5GDNSP4\Joao <joao.silva@simberg.com.br>
2022-06-27 06:56:48 -0300

    test: ensure Min length validation return falsy if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,18 +1,16 @@
 import { InvalidFieldError } from '@/validation/errors'
-import { FieldValidation } from '@/validation/protocols/field-validation'
-
-class MinLengthValidation implements FieldValidation {
-  constructor (readonly field: string, private readonly minLength: number) {}
-
-  validate (value: string): Error {
-    return new InvalidFieldError()
-  }
-}
+import { MinLengthValidation } from './min-length-validation'
 
 describe('Min length validation', () => {
   test('Should return error if value is invalid', () => {
     const sut = new MinLengthValidation('field', 5)
     const error = sut.validate('123')
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if value is valid', () => {
+    const sut = new MinLengthValidation('field', 5)
+    const error = sut.validate('12345')
+    expect(error).toBeFalsy()
   })
 })
