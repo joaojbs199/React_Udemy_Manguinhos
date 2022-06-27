51a2c68a91a4fd58822cc8cee5b51858f814bea9
DESKTOP-5GDNSP4\Joao <joao.silva@simberg.com.br>
2022-06-27 12:08:25 -0300

    feat: ensure validation composite returns error if any validation fails

---
+++ src/validation/validators/validation-composite/validation-composite.ts
@@ -0,0 +1,14 @@
+import { Validation } from '@/presentation/protocols/validation'
+import { FieldValidation } from '../../protocols/field-validation'
+
+export class ValidationComposite implements Validation {
+  constructor (private readonly validators: FieldValidation[]) {}
+
+  validate (field: string, value: string): string {
+    const validators = this.validators.filter(v => v.field === field)
+    for (const validator of validators) {
+      const error = validator.validate(value)
+      if (error) return error.message
+    }
+  }
+}
