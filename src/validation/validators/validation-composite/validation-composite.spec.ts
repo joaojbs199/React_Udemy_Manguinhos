7903b6215d104087761839f863a659cc5f2a6834
DESKTOP-5GDNSP4\Joao <joao.silva@simberg.com.br>
2022-06-27 12:14:03 -0300

    test: ensure validation composite returns the first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,13 +4,14 @@ import { ValidationComposite } from './validation-composite'
 describe('Validation composite', () => {
   test('Should return error if any validator fails', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field')
+    fieldValidationSpy.error = new Error('first_error')
     const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('any_errorMessage')
+    fieldValidationSpy2.error = new Error('second_error')
     const sut = new ValidationComposite([
       fieldValidationSpy,
       fieldValidationSpy2
     ])
     const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('any_errorMessage')
+    expect(error).toBe('first_error')
   })
 })
