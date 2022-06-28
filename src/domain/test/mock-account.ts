0c9934ab1f60f45decda3fb4466d9fdcc408c4d4
DESKTOP-5GDNSP4\Joao <joao.silva@simberg.com.br>
2022-06-28 15:42:33 -0300

    chore: add files to coverage ignore

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,6 +1,6 @@
 import { AuthenticationParams } from '@/domain/useCases'
 import faker from 'faker'
-import { AccountModel } from '../models'
+import { AccountModel } from '@/domain/models'
 
 export const mockAuthentication = (): AuthenticationParams => ({
   email: faker.internet.email(),
